df26b7f115d2c08c95af5947ed4e91d29bd9d548
AJustAiden <146122124+AJustAiden@users.noreply.github.com>
2023-09-26 14:18:53 -0400

    Add files via upload

---
+++ P1HW1_AAiden.py
@@ -0,0 +1,15 @@
+#9/26/23
+#Input,output,expressions
+
+num1= int(input("Enter a number: "))
+
+print("you entered: ",num1)
+
+print(num1, "squared is", num1**2)
+
+print("and",num1, "cubed is", num1**3)
+
+num2 = int(input("enter a number: "))
+
+print(num1, "+" , num2,"=",num1+num2)
+print(num1, "*", num2, "=",num1*num2)
