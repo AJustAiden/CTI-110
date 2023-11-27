4cb1d3b4192f1b97f1a2687705e5159ad57dd7dd
AllisonAiden <146122124+AJustAiden@users.noreply.github.com>
2023-11-26 21:46:16 -0500

    Update P5HW_AllisonAiden.py

--- P5HW_AllisonAiden.py
+++ P5HW_AllisonAiden.py
@@ -1,68 +1,69 @@
-#Aiden Allison
-#11/16/23
-#Using if else staments, loops , funcations and random numbers
+# Aiden Allison
+# 11/16/23
+# Using if else statements, loops , functions and random numbers
 import random
 
+
 def Adding():
-    num_1 = random.randint(0,10)
-    num_2 = random.randint(0,10)
-    Sum = num_1 + num_2 
-    Answer = int(input(f"What is the sum of {num_1} + {num_2}: "))
+    num_1 = random.randint(0, 10)
+    num_2 = random.randint(0, 10)
+    global Sum
+    Sum = num_1 + num_2
+
+
+def Minus():
+    num_1 = random.randint(0, 10)
+    num_2 = random.randint(0, 10)
+    global Sum
+    Sum = num_1 - num_2
+
+
+def guessing():
     guessCount = 1
+    Answer = int(input(f"What is your guess? "))
     while Answer != Sum:
         if Answer > Sum:
             print("your guess was too high: ")
-            Answer = int(input(f"What is the sum of {num_1} + {num_2}: "))
+            Answer = int(input(f"What is your guess? "))
             guessCount += 1
         elif Answer < Sum:
             print("your guess was too low: ")
-            Answer = int(input(f"What is the sum of {num_1} + {num_2}: "))
+            Answer = int(input(f"What is your guess? "))
             guessCount += 1
     print("good job you got it right!!")
     print(f"you guessed {guessCount} times")
 
-def Minus():
-    num_1 = random.randint(0,10)
-    num_2 = random.randint(0,10)
-    Sum = num_1 - num_2
-    Answer = int(input(f"What is the sum of {num_1} - {num_2}: "))
-    guessCount = 1
-    while Answer != Sum:
-        if Answer > Sum:
-            print("your guess was too high: ")
-            Answer = int(input(f"What is the sum of {num_1} - {num_2}: "))
-            guessCount += 1
-        elif Answer < Sum:
-            print("your guess was too low: ")
-            Answer = int(input(f"What is the sum of {num_1} - {num_2}: "))
-            guessCount += 1
-    print("good job you got it right!!")
-    print(f"you guessed {guessCount} times")
-def guessing():
-    
-    
 def menu():
     print("MAIN MENU")
     print("-------------------------------")
     print("1.Adding Random numbers")
     print("2.Subtracting Random Numbers")
     print("3.Exit")
+    global menuNum
+    menuNum = int(input("Please choose one of the menu options: "))
+
 def Exit():
     ("program over")
-#mainfunction that runs program
+
+
+# mainfunction that runs program
 def main():
     menu()
-    menuNum = int(input("Please choose one of the menu options: "))
 
     if menuNum == 1:
         Adding()
+        print(Sum)
+        guessing()
     elif menuNum == 2:
         Minus()
+        print(Sum)
+        guessing()
     elif menuNum == 3:
         Exit()
     else:
-         print("error wrong number")
-         menu()
-#Call the mainfunction
+        print("error none usable number")
+        menu()
+    exit()
+# Call the main function
 if __name__ == "__main__":
     main()
